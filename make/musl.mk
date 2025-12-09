008f5fdda0916f220c4bed0b62d644227d167dbd
Sebastiano Trombetta <strombetta@users.noreply.github.com>
2025-12-09 13:53:53 +0100

    Fix musl unpack target reference

--- make/musl.mk
+++ make/musl.mk
@@ -4,7 +4,8 @@
 # Licensed under the MIT License. See LICENSE file for details.
 ##
 
-include $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/common.mk)
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+include $(abspath $(dir $(THIS_MAKEFILE))/common.mk)
 
 .PHONY: all
 all: musl
@@ -15,14 +16,14 @@ musl: ensure-dirs $(MUSL_BUILD_DIR)/.built-musl
 $(MUSL_BUILD_DIR)/.built-musl: $(MUSL_ARCHIVE)
 	@echo "[musl] Building musl libc for $(TARGET)"
 	@mkdir -p $(MUSL_BUILD_DIR)
-	@$(MAKE) unpack-musl
+	@$(MAKE) -f $(THIS_MAKEFILE) unpack-musl
 	@cd $(MUSL_SRC_DIR) && CC="$(TARGET)-gcc" ./configure \
-	    --prefix=/usr \
-	    --target=$(TARGET) \
-	    --host=$(TARGET) \
-	    --enable-wrapper=no \
-	    --syslibdir=/lib \
-	    > $(LOGS_DIR)/musl-configure.log 2>&1
+	--prefix=/usr \
+	--target=$(TARGET) \
+	--host=$(TARGET) \
+	--enable-wrapper=no \
+	--syslibdir=/lib \
+	> $(LOGS_DIR)/musl-configure.log 2>&1
 	@$(MAKE) -C $(MUSL_SRC_DIR) DESTDIR=$(SYSROOT) install-headers > $(LOGS_DIR)/musl-headers.log 2>&1
 	@$(MAKE) -C $(MUSL_SRC_DIR) -j$(JOBS) CROSS_COMPILE=$(TARGET)- > $(LOGS_DIR)/musl-build.log 2>&1
 	@$(MAKE) -C $(MUSL_SRC_DIR) DESTDIR=$(SYSROOT) install > $(LOGS_DIR)/musl-install.log 2>&1
