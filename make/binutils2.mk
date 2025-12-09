b0ab57c7c7182786adafb76f1e183cc4832f4918
Sebastiano Trombetta <strombetta@users.noreply.github.com>
2025-12-09 10:05:33 +0100

    Normalize binutils makefile indentation

--- make/binutils2.mk
+++ make/binutils2.mk
@@ -4,6 +4,7 @@
 # Licensed under the MIT License. See LICENSE file for details.
 ##
 
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
 include $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/common.mk)
 
 .PHONY: all
@@ -15,15 +16,15 @@ binutils2: ensure-dirs $(BINUTILS2_BUILD_DIR)/.built-stage2
 $(BINUTILS2_BUILD_DIR)/.built-stage2: $(BINUTILS_ARCHIVE)
 	@echo "[binutils2] Rebuilding binutils against sysroot for $(TARGET)"
 	@mkdir -p $(BINUTILS2_BUILD_DIR)
-	@$(MAKE) unpack-binutils
+	@$(MAKE) -f $(THIS_MAKEFILE) unpack-binutils
 	@cd $(BINUTILS2_BUILD_DIR) && $(BINUTILS_SRC_DIR)/configure \
-	    --target=$(TARGET) \
-	    --prefix=$(TOOLCHAIN_DIR) \
-	    --with-sysroot=$(SYSROOT) \
-	    --disable-nls \
-	    --disable-werror \
-	    --enable-deterministic-archives \
-	    > $(LOGS_DIR)/binutils2-configure.log 2>&1
+	--target=$(TARGET) \
+	--prefix=$(TOOLCHAIN_DIR) \
+	--with-sysroot=$(SYSROOT) \
+	--disable-nls \
+	--disable-werror \
+	--enable-deterministic-archives \
+	> $(LOGS_DIR)/binutils2-configure.log 2>&1
 	@$(MAKE) -C $(BINUTILS2_BUILD_DIR) -j$(JOBS) > $(LOGS_DIR)/binutils2-build.log 2>&1
 	@$(MAKE) -C $(BINUTILS2_BUILD_DIR) install > $(LOGS_DIR)/binutils2-install.log 2>&1
 	@touch $@
