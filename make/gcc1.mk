d34346a4161946e81d4b262df90260d124350f45
Sebastiano Trombetta <s.trombetta@live.com>
2025-12-10 09:12:42 +0100

    Fixed copyright header and add absolute toolchain path for gcc1

--- make/gcc1.mk
+++ make/gcc1.mk
@@ -1,11 +1,36 @@
-##
 # Copyright (c) 2025 Sebastiano Trombetta
-# SPDX-License-Identifier: MIT
-# Licensed under the MIT License. See LICENSE file for details.
-##
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
 include $(abspath $(dir $(THIS_MAKEFILE))/common.mk)
+
+TOOLCHAIN_BIN := $(TOOLCHAIN_DIR)/bin
+PATH := $(TOOLCHAIN_BIN):$(PATH)
+export PATH
+export NM_FOR_TARGET			:= $(TARGET)-nm
+export AR_FOR_TARGET			:= $(TARGET)-ar
+export AS_FOR_TARGET			:= $(TARGET)-as
+export LD_FOR_TARGET			:= $(TARGET)-ld
+export RANLIB_FOR_TARGET	:= $(TARGET)-ranlib
+export OBJDUMP_FOR_TARGET	:= $(TARGET)-objdump
+export OBJCOPY_FOR_TARGET	:= $(TARGET)-objcopy
 
 .PHONY: all
 all: gcc1
