7599b22c0569cd33fc031115b930994cb5671d52
strombetta <12155693+strombetta@users.noreply.github.com>
2025-12-10 22:12:36 +0100

    Update gcc2.mk

--- make/gcc2.mk
+++ make/gcc2.mk
@@ -32,7 +32,7 @@ $(GCC_BUILD_DIR)/.built-gcc2: $(GCC_ARCHIVE)
 	@echo "[gcc2] Building final GCC for $(TARGET)"
 	@rm -rf $(GCC_BUILD_DIR)
 	@mkdir -p $(GCC_BUILD_DIR)
-	@$(MAKE) unpack-gcc2
+	@$(MAKE) unpack-gcc
 	@cd $(GCC_SRC_DIR) && ./contrib/download_prerequisites > $(LOGS_DIR)/gcc2-prereqs.log 2>&1 || true
 	@cd $(GCC_BUILD_DIR) && $(GCC_SRC_DIR)/configure \
 	    --target=$(TARGET) \
@@ -41,9 +41,9 @@ $(GCC_BUILD_DIR)/.built-gcc2: $(GCC_ARCHIVE)
 	    --enable-languages=c,c++ \
 	    --disable-nls \
 	    --disable-multilib \
-	    --enable-checking=release \
 	    --disable-libsanitizer \
 	    --disable-libitm \
+	    --enable-checking=release \
 	    --enable-threads=posix \
 	    > $(LOGS_DIR)/gcc2-configure.log 2>&1
 	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) > $(LOGS_DIR)/gcc2-build.log 2>&1
