672fa9e01150c7cf8240d924c395ba68d1253748
strombetta <12155693+strombetta@users.noreply.github.com>
2025-12-10 16:34:12 +0100

    Update common.mk

--- make/common.mk
+++ make/common.mk
@@ -17,19 +17,16 @@ PKGDIR ?= $(ROOT_DIR)/patches
 
 BINUTILS_ARCHIVE := $(DOWNLOADS_DIR)/binutils-$(BINUTILS_VERSION).tar.xz
 BINUTILS_SRC_DIR := $(SOURCES_DIR)/binutils-$(BINUTILS_VERSION)
-
 GCC_ARCHIVE := $(DOWNLOADS_DIR)/gcc-$(GCC_VERSION).tar.xz
 GCC_SRC_DIR := $(SOURCES_DIR)/gcc-$(GCC_VERSION)
-
 MUSL_ARCHIVE := $(DOWNLOADS_DIR)/musl-$(MUSL_VERSION).tar.gz
 MUSL_SRC_DIR := $(SOURCES_DIR)/musl-$(MUSL_VERSION)
 
 # Directory helpers
 BINUTILS1_BUILD_DIR := $(BUILDS_DIR)/binutils-stage1
-GCC1_BUILD_DIR := $(BUILDS_DIR)/gcc-stage1
+GCC_BUILD_DIR := $(BUILDS_DIR)/gcc
 MUSL_BUILD_DIR := $(BUILDS_DIR)/musl
 BINUTILS2_BUILD_DIR := $(BUILDS_DIR)/binutils-stage2
-GCC2_BUILD_DIR := $(BUILDS_DIR)/gcc-final
 
 # Tools
 MAKEFLAGS += --no-builtin-rules
@@ -42,11 +39,7 @@ unpack-binutils:
 	@rm -rf $(BINUTILS_SRC_DIR)
 	@$(TAR) -xf $(BINUTILS_ARCHIVE) -C $(SOURCES_DIR)
 
-unpack-gcc1:
-	@rm -rf $(GCC_STAGE1_SRC_DIR)
-	@$(TAR) -xf $(GCC_STAGE1_ARCHIVE) -C $(SOURCES_DIR)
-
-unpack-gcc2:
+unpack-gcc:
 	@rm -rf $(GCC_SRC_DIR)
 	@$(TAR) -xf $(GCC_ARCHIVE) -C $(SOURCES_DIR)
 
--- make/gcc1.mk
+++ make/gcc1.mk
@@ -25,15 +25,16 @@ include $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/common.mk)
 all: gcc1
 
 .PHONY: gcc1
-gcc1: ensure-dirs $(GCC1_BUILD_DIR)/.built-stage1
+gcc1: ensure-dirs $(GCC_BUILD_DIR)/.built-stage1
 
-$(GCC1_BUILD_DIR)/.built-stage1: $(GCC_ARCHIVE)
+$(GCC_BUILD_DIR)/.built-stage1: $(GCC_ARCHIVE)
 	@echo "[BugleOS] Building GNU GCC v$(GCC_VERSION) for $(TARGET)"
 	@echo "[Debug] Path=$(PATH)"
-	@mkdir -p $(GCC1_BUILD_DIR)
-	@$(MAKE) -f $(THIS_MAKEFILE) unpack-gcc1
+	@rm -rf $(GCC_BUILD_DIR)
+	@mkdir -p $(GCC_BUILD_DIR)
+	@$(MAKE) -f $(THIS_MAKEFILE) unpack-gcc
 	@cd $(GCC_SRC_DIR) && ./contrib/download_prerequisites > $(LOGS_DIR)/gcc1-prereqs.log 2>&1 || true
-	@cd $(GCC1_BUILD_DIR) && $(GCC_SRC_DIR)/configure \
+	@cd $(GCC_BUILD_DIR) && $(GCC_SRC_DIR)/configure \
 		--target=$(TARGET) \
 		--prefix=$(TOOLCHAIN) \
 		--with-sysroot=$(SYSROOT) \
@@ -54,9 +55,9 @@ $(GCC1_BUILD_DIR)/.built-stage1: $(GCC_ARCHIVE)
 		--enable-languages=c \
 		--enable-checking=release \
 		> $(LOGS_DIR)/gcc1-configure.log 2>&1
-	@$(MAKE) -C $(GCC1_BUILD_DIR) -j$(JOBS) all-gcc > $(LOGS_DIR)/gcc1-build.log 2>&1
-	@$(MAKE) -C $(GCC1_BUILD_DIR) -j$(JOBS) all-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-build.log 2>&1
-	@$(MAKE) -C $(GCC1_BUILD_DIR) install-gcc > $(LOGS_DIR)/gcc1-install.log 2>&1
-	@$(MAKE) -C $(GCC1_BUILD_DIR) install-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-install.log 2>&1
+	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) all-gcc > $(LOGS_DIR)/gcc1-build.log 2>&1
+	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) all-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-build.log 2>&1
+	@$(MAKE) -C $(GCC_BUILD_DIR) install-gcc > $(LOGS_DIR)/gcc1-install.log 2>&1
+	@$(MAKE) -C $(GCC_BUILD_DIR) install-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-install.log 2>&1
 	@touch $@
 
