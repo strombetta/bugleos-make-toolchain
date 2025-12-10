6078e2fd635cb8c6cbd9d0a62cb6fe9060b5594f
strombetta <12155693+strombetta@users.noreply.github.com>
2025-12-10 17:43:01 +0100

    Enable install-target-libgcc

--- make/gcc1.mk
+++ make/gcc1.mk
@@ -58,6 +58,5 @@ $(GCC_BUILD_DIR)/.built-stage1: $(GCC_ARCHIVE)
 	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) all-gcc > $(LOGS_DIR)/gcc1-build.log 2>&1
 	@$(MAKE) -C $(GCC_BUILD_DIR) install-gcc > $(LOGS_DIR)/gcc1-install.log 2>&1
 	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) all-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-build.log 2>&1
-	#@$(MAKE) -C $(GCC_BUILD_DIR) install-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-install.log 2>&1
+	@$(MAKE) -C $(GCC_BUILD_DIR) install-target-libgcc > $(LOGS_DIR)/gcc1-libgcc-install.log 2>&1
 	@touch $@
-
