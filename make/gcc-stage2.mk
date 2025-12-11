55545c30d8b3f9595ffc0b216af0f8a9cba90433
Sebastiano Trombetta <strombetta@users.noreply.github.com>
2025-12-11 11:29:35 +0100

    Update gcc-stage2.mk

--- make/gcc-stage2.mk
+++ make/gcc-stage2.mk
@@ -29,25 +29,39 @@ all: gcc-stage2
 gcc-stage2: ensure-dirs $(GCC_BUILD_DIR)/.built-gcc-stage2
 
 $(GCC_BUILD_DIR)/.built-gcc-stage2: $(GCC_ARCHIVE)
-	@echo "[gcc-stage2] Building final GCC for $(TARGET)"
-	@rm -rf $(GCC_BUILD_DIR)
-	@mkdir -p $(GCC_BUILD_DIR)
-	@$(MAKE) -f $(THIS_MAKEFILE) unpack-gcc
-	@cd $(GCC_SRC_DIR) && ./contrib/download_prerequisites > $(LOGS_DIR)/gcc-stage2-prereqs.log 2>&1 || true
-	@cd $(GCC_BUILD_DIR) && $(GCC_SRC_DIR)/configure \
-	    --target=$(TARGET) \
-	    --prefix=$(TOOLCHAIN) \
-	    --with-sysroot=$(SYSROOT) \
-		 --with-native-system-header-dir=/usr/include \
-	    --enable-languages=c,c++ \
-	    --disable-nls \
-	    --disable-multilib \
-	    --disable-libsanitizer \
-	    --disable-libitm \
-		 --disable-libstdcxx-backtrace \
-	    --enable-checking=release \
-	    --enable-threads=posix \
-	    > $(LOGS_DIR)/gcc-stage2-configure.log 2>&1
-	@$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS) > $(LOGS_DIR)/gcc-stage2-build.log 2>&1
-	@$(MAKE) -C $(GCC_BUILD_DIR) install > $(LOGS_DIR)/gcc-stage2-install.log 2>&1
-	@touch $@
+	$(Q)rm -rf $(GCC_BUILD_DIR)
+	S(Q)mkdir -p $(GCC_BUILD_DIR)
+
+	$(call do_step,EXTRACT,gcc-stage2, \
+		$(MAKE) -f $(THIS_MAKEFILE) unpack-gcc, \
+		gcc-stage2-extract)
+
+	$(call do_step,EXTRACT,gcc-stage1-prerequisites, \
+		cd $(GCC_SRC_DIR) && ./contrib/download_prerequisites, \
+		gcc-stage2-prereqs)
+
+	$(call do_step,CONFIG,gcc-stage2, \
+		cd $(GCC_BUILD_DIR) && $(GCC_SRC_DIR)/configure \
+		    --target=$(TARGET) \
+		    --prefix=$(TOOLCHAIN) \
+		    --with-sysroot=$(SYSROOT) \
+			 --with-native-system-header-dir=/usr/include \
+		    --enable-languages=c,c++ \
+		    --disable-nls \
+		    --disable-multilib \
+		    --disable-libsanitizer \
+		    --disable-libitm \
+			 --disable-libstdcxx-backtrace \
+		    --enable-checking=release \
+		    --enable-threads=posix, \
+			gcc-stage2-configure)
+
+	$(call do_step,BUILD,gcc-stage2, \
+		$(MAKE) -C $(GCC_BUILD_DIR) -j$(JOBS), \
+		gcc-stage2-build)
+	
+	$(call do_step,INSTALL,gcc-stage2, \
+		$(MAKE) -C $(GCC_BUILD_DIR) install, \
+		gcc-stage2-install)
+	
+	$(Q)touch $@
